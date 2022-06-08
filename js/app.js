76396eed5eeba2ba19acc2f2c7b5ea528a111224
Bulat <karimovMINDS@gmail.com>
2022-06-08 05:46:26 +0400

    updated content

--- js/app.js
+++ js/app.js
@@ -1,15 +1,15 @@
 $(function () {
 	//mixer category helper
-	var mixer = mixitup('.portfolio__content', {
+	let mixer = mixitup(".portfolio__content", {
 		animation: {
 			duration: 500,
-			effectsIn: 'fade translateX(-100px)',
-			effectsOut: 'fade translateY(-150px)',
+			effectsIn: "fade translateX(-100px)",
+			effectsOut: "fade translateY(-150px)",
 		},
 	});
 
 	//slick
-	$('.slider__inner').slick({
+	$(".slider__inner").slick({
 		dots: true,
 		arrows: false,
 		speed: 800,
@@ -19,36 +19,36 @@ $(function () {
 	});
 
 	//header menu < 968px
-	let menuBtn = $('.menu__btn'),
-		menuBtnC = $('.menu__list-btn'),
-		list = $('.menu__list');
+	let menuBtn = $(".menu__btn"),
+		menuBtnC = $(".menu__list-btn"),
+		list = $(".menu__list");
 
-	list.removeClass('menu__list--active');
+	list.removeClass("menu__list--active");
 
-	menuBtn.on('click', function () {
-		list.addClass('menu__list--active');
-		menuBtn.css('display', 'none');
+	menuBtn.on("click", function () {
+		list.addClass("menu__list--active");
+		menuBtn.css("display", "none");
 	});
 
-	menuBtnC.on('click', function () {
-		list.removeClass('menu__list--active');
-		menuBtn.css('display', 'block');
+	menuBtnC.on("click", function () {
+		list.removeClass("menu__list--active");
+		menuBtn.css("display", "block");
 	});
 
-	$('.menu a, .logo').on('click', function (event) {
+	$(".menu a, .logo").on("click", function (event) {
 		event.preventDefault();
 
-		var id = $(this).attr('href'),
-			top = $(id).offset().top;
+		let id = $(this).attr("href");
+		let top = $(id).offset().top;
 
-		$('body,html').animate({ scrollTop: top }, 1000);
+		$("body,html").animate({ scrollTop: top }, 1000);
 	});
 
 	$(window).scroll(function () {
-		let header = $('.header__top');
+		let header = $(".header__top");
 
 		if (scrollY > window.innerHeight) {
-			header.addClass('fixed');
-		} else header.removeClass('fixed');
+			header.addClass("fixed");
+		} else header.removeClass("fixed");
 	});
 });
